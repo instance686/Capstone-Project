3aaab65faa422a9a0e285912791a7ad88ef535ce
ayush <ayush.cir@gmail.com>
2018-01-05 02:46:31 +0530

    Chat and Reminder Layouts Created

---
+++ app/src/main/java/com/reminders/location/locatoinreminder/view/ui/activity/ReminderSet.java
@@ -0,0 +1,15 @@
+package com.reminders.location.locatoinreminder.view.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.reminders.location.locatoinreminder.R;
+
+public class ReminderSet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_set);
+    }
+}
